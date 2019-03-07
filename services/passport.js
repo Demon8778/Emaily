88d43b42404af612620b322dc84008ca408f076b
Kirankumar Ahir <kirankatariya8778@gmail.com>
2019-03-08 00:18:22 +0530

    Finished Auth Flow

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,20 @@
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("../config/keys");
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+});
 
 passport.use(
     new GoogleStrategy(
@@ -10,11 +24,25 @@ passport.use(
             callbackURL: "/auth/google/callback"
         },
         (accessToken, refreshToken, profile, done) => {
-            console.log(accessToken);
+            // console.log(accessToken);
+            // console.log(profile.id);
+            User.findOne({ googleId: profile.id })
+                .then((existingUser) => {
+                    if(existingUser) {
+                        //We already have a record with the given profile id
+                        done(null, existingUser);
+                    } else {
+                        //we don't have a user so we have to create a 
+                        new User({ googleId: profile.id })
+                            .save()
+                            .then(user => done(null, user));
+                    }
+                });
+            
         }
     )
 );
 
-module.exports = {
+// module.exports = {
     
-}
+// }
